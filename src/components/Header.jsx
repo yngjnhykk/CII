6bfddf2ddd5d47b1448a33cb65af91f8a2c8996e
jinhyeok99 <yngjnhyk@gmail.com>
2024-06-21 11:07:33 +0900

    add: pages, components

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div
+      className={`m-0 py-8 bg-red-200 text-center h-16`}
+      style={{ height: "100px" }}
+    >
+      Header
+    </div>
+  );
+}
+
+export default Header;
