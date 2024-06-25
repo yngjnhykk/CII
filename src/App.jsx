d5b14b627ba7dc5076b76848260fb08d3c58c279
qazplm12 <yeah_chef@naver.com>
2024-06-25 13:47:49 +0900

    ws_branch 수정

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,26 @@ import { BrowserRouter } from "react-router-dom";
 import Router from "./Router";
 import Header from "./components/Header";
 
+
+
+
 function App() {
   return (
     <>
       <Header />
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <div style={{ width: 1200 }}>
+          <BrowserRouter>
+            <Router />
+          </BrowserRouter>
+        </div>
+      </div>
     </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,27 @@
 import React from "react";
+import styled from "styled-components";
 
 function Header() {
   return (
-    <div
-      className={`m-0 py-8 bg-red-200 text-center h-16`}
-      style={{ height: "100px" }}
-    >
-      Header
-    </div>
+    <Wrap>
+      <div style={{ fontSize: 32 }}>CII</div>
+      <div style={{ display: "flex", gap: 50, fontSize: 20 }}>
+        <div>선박 관리</div>
+        <div>시뮬레이터</div>
+      </div>
+    </Wrap>
   );
 }
 
 export default Header;
+
+const Wrap = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #0c1427;
+  color: white;
+  height: 70px;
+  padding: 0px 120px;
+`;
